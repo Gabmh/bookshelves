97f1b2f86eb8f7daf3c1594c71359211db6ef93e
Gabriel Mahé <mahegabriel@gmail.com>
2020-11-25 19:46:22 +0100

    création du skelette de l'application + authentification complète

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyC1kpzDXqMy7LsJmeQlucc9omTk8G5GgCI',
+      authDomain: 'bookshelves-a7668.firebaseapp.com',
+      databaseURL: 'https://bookshelves-a7668.firebaseio.com',
+      projectId: 'bookshelves-a7668',
+      storageBucket: 'bookshelves-a7668.appspot.com',
+      messagingSenderId: '100693665717',
+      appId: '1:100693665717:web:0fe2991d279d9ae2b65bfc',
+      measurementId: 'G-6ZJ3ZJD89M'
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
